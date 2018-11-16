f94d878d22232e8fae6e8c23d3db64d5bceee82b
AndoBando <andreas.badea@gmail.com>
2018-11-15 22:01:53 -0500

    added deck class and shuffling

---
+++ Deck.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andy on 11/15/2018.
+//
+
+#include "Deck.h"
---
+++ Deck.h
@@ -0,0 +1,14 @@
+//
+// Created by andy on 11/15/2018.
+//
+
+#ifndef WHIST_DECK_H
+#define WHIST_DECK_H
+
+
+class Deck {
+
+};
+
+
+#endif //WHIST_DECK_H
